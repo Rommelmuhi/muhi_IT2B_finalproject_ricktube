ebe8ce563c63e7db9cac599841d71a8c7d46ecb6
RommelMuhi <jromdaily@gmail.com>
2022-05-12 12:13:43 +0800

    added episode plot s1-2

---
+++ s2-e10.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>RickTube</title>
+        <link rel="icon" type="image/x-icon" href="assets/ram1.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> RickTube </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Season 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="season2.php">Season 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="season3.php">Season 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="season4.php">Season 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="season5.php">Season 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-black bg-gradient text-white">
+            <div class="container px-4 text-center">
+
+                <h1 class="fw-bolder">RickTube</h1>
+                <p>"Season 1"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12">
+
+                        <div class="row">
+                            <div class=""><img src="images/s2/e10.jpg" alt="" class="img-fluid rounded shadow-sm">
+                                
+                                <p class="lead">The Wedding Squanchers - Season 2 Episode 10</p>
+                              
+
+                            </div>
+
+                            <div>
+                                <h2>Plot</h2>
+                                <p>October 4, 2015</p>
+                                <h4>The Smith family is invited to the wedding of Rick's best friend, Birdperson, but things go south when it's discovered that the bride isn't who she says she is.</h4>
+                            </div>
+                        </div>
+
+<br>
+<br>
+                        <div class="row">
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e1.jpg" alt="" class="img-fluid rounded shadow-sm">
+
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e1.php">Video here..</a></h2>
+                                <p class="lead">A Rickle in Time</p>
+                                <p>Season 2 Episode 1</p>
+
+                            </div>
+                            
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e2.jpg" alt="" class="img-fluid rounded shadow-sm">
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e2.php">Video here..</a></h2>
+                                <p class="lead">Mortynight Run</p>
+                                <p>Season 2 Episode 2</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e3.jpg" alt="" class="img-fluid rounded shadow-sm">
+
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e3.php">Video here..</a></h2>
+                                <p class="lead">Auto Erotic Assimilation</p>
+                                <p>Season 2 Episode 3</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e4.png" alt="" class="img-fluid rounded shadow-sm">
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e4.php">Video here..</a></h2>
+                                <p class="lead">Total Rickall</p>
+                                <p>Season 2 Episode 4</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e5.jpg" alt="" class="img-fluid rounded shadow-sm">
+
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e5.php">Video here..</a></h2>
+                                <p class="lead">Get Schwifty</p>
+                                <p>Season 2 Episode 5</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e6.jpg" alt="" class="img-fluid rounded shadow-sm">
+                               <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e6.php">Video here..</a></h2>
+                                <p class="lead">The Ricks Must Be Crazy</p>
+                                <p>Season 2 Episode 6</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e7.jpg" alt="" class="img-fluid rounded shadow-sm">
+
+                               <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e7.php">Video here..</a></h2>
+                                <p class="lead">Big Trouble in Little Sanchez</p>
+                                <p>Season 2 Episode 7</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e8.jpg" alt="" class="img-fluid rounded shadow-sm">
+                               <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e8.php">Video here..</a></h2>
+                                <p class="lead">Interdimensional Cable 2: Tempting Fate</p>
+                                <p>Season 2 Episode 8</p>
+
+                            </div>
+                            <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/s2/e9.jpg" alt="" class="img-fluid rounded shadow-sm">
+
+                                <h2 class="text-primary"><a class="nav-link text-blue" href="s2-e9.php">Video here..</a></h2>
+                                <p class="lead">Look Who's Purging Now</p>
+                                <p>Season 2 Episode 9</p>
+
+                            </div>
+                            
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jhon Rommel N. Muhi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
